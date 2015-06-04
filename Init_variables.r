720798037083cabdd763297e48ea6b8c5ef4d7c8
Fernando Moyano <fmoyano@uni-goettingen.de>
2015-06-04 17:51:02 +0200

    Deleted.

--- Init_variables.r
+++
@@ -1,8 +0,0 @@
-# Init_variables.r
-
-# Function to check if a file for initial values of state variables exists, else set initial values to 0
-
-Init_values <- function(file) {
-  ifelse (file.exits(file), load.csv(file), assign(file, 0))
-  ifelse(get(file) == 0, print(paste("No initial data found for ", file, ". Initial values set to 0.", sep="")))
-}
