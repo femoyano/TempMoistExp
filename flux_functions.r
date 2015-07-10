8336f3d70868af449e2fa70bea3fa6d63cf71c1b
Fernando Moyano <fmoyano@uni-goettingen.de>
2015-07-10 12:04:05 +0200

    File names

--- flux_functions.r
+++ flux_functions.r
@@ -1,7 +1,8 @@
 # flux_functions.r 
 
 # Documentation
-# Note: chemical reactions occur in the water phase, so C pools are  divided by water
+# Note: chemical reactions occur in the water phase and are calulated per cm^-3 water
+# so C pools are  divided by water times cm^3 in 1m^3 water.
 # content to obtain concentrations, then rates are multiplied by water content.
 
 # Functions calculating the fluxes of C.
@@ -12,35 +13,14 @@ F_litter <- function (litter_flux) { # the input of litter is prescribed; no cal
 }
 
 # Decomposition flux
-F_decomp <- function (C, E, V, K, theta, cm3) {
-  C <- C / (theta * cm3)
-  E <- E / (theta * cm3)
-  D <- (V * C * E) / (K + C + E) * (theta * cm3)
+F_decomp <- function (C, E, V, K) {
+  D <- (V * C * E) / (K + C + E)
   ifelse(D > C, C, D) # max decomp is size of C, so C cannot become negative
 }
 
-# Sorption to mineral surface
-F_sorp <- function (SCw, SCs, Ew, Es, M, K_M1, K_M2, theta, cm3) {
-  SC <- SCw + SCs
-  E <- Ew + Es
-  SC <- SC / (theta * cm3)
-  E <- E / (theta * cm3)
-  M <- M / (theta * cm3)
-  (SC * M) / (K_M1 * (1 + SC / K_M1 + M / K_M1 + E / K_M2)) * (theta * cm3) - SCs
-}
-
-# Diffusion flux
-F_diff <- function (Sw, Sm, D_S0, theta_s, dist, phi, Rth) {
-  if (theta_s <= Rth) return(0)
-  D_S <- D_S0 * (phi - Rth)^1.5 * ((theta_s - Rth)/(phi - Rth))^2.5
-  D_S * (Sw - Sm) / dist # dividing by theta for specific concentrations and multiplying again for total cancels theta out
-}
-
 # Microbial C uptake
-F_uptake <- function (SC, MC, theta, V_U, K_U, cm3) {
-  SC <- SC / (theta * cm3)
-  MC <- MC / (theta * cm3)
-  U <- (V_U * SC * MC) / (K_U + SC + MC) * (theta * cm3)
+F_uptake <- function (SC, MC, V_U, K_U) {
+  U <- (V_U * SC * MC) / (K_U + SC)
   ifelse(U > SC, SC, U) # max uptake is size of SC, so SC cannot become negative
 }
 
@@ -60,27 +40,9 @@ F_mc.pc <- function (MC, Mm, mcsc_f) {
 }
 
 # Enzymes decay
-F_ecw.scw <- function (ECw, Em) {
-  ECw * Em
+F_ec.sc <- function (EC, Em) {
+  EC * Em
 }
-
-# Transfer from / to immobile pool
-F_scw.sci <- function (SCw, SCi, theta_d, theta, theta_fc) {
-  if (theta < theta_fc) {
-    ifelse (theta_d >= 0, max(-theta_d * (SCi / (theta_fc - theta)), -SCi), -theta_d * (SCw / theta))
-  } else -SCi
-}
-
-# Advection flux out
-F_scw.adv <- function (SCw, theta, percolation) {
-  SCw / theta * percolation
-}
-
-# Advection flux in
-F_adv.scw <- function (advection_in) { # prescribed
-  advection_in
-}
-
 
 # ==============================================================================
 # Temperature responses after to Tang and Riley 2014 (supplementary information)
@@ -97,10 +59,10 @@ Temp.Resp.NonEq <- function(K, T, T0, E, R) {
 
 # ==============================================================================
 # Function to check for equilibirum conditions
-CheckEquil <- function(PC, i, eq.mpd) {
+CheckEquil <- function(PC, i, eq.md) {
   y1 <- PC[(i-12/delt+1):i]
   y2 <- PC[(i-24/delt+1):(i-12/delt)]
-  x <- abs((mean(y1) / mean(y2)) - 1)
-  ifelse(x <= (eq.mpd/100), TRUE, FALSE)
+  x <- abs(mean(y2) - mean(y1))
+  ifelse(x <= (eq.md), TRUE, FALSE)
 }
 
