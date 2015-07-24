a6873b3537a6249367c2f9290f96988b2a338b18
Fernando Moyano <fmoyano@uni-goettingen.de>
2015-07-24 11:47:17 +0200

    load input calculates fc

--- load_inputs.R
+++ load_inputs.R
@@ -8,23 +8,30 @@
 # Litter input must be in gC m-2 d-1
 ### ============================================================================
 
-data.time <- hour
+### Spatial Variables ==========================================================
+sand   <- 0.04  # [g g^-1] clay fraction values 
+clay   <- 0.90  # [g g^-1] sand fraction values 
+silt   <- 0.06  # [g g^-1] silt fraction values 
+phi    <- 0.5   # [m^3 m^-3] soil pore space
+depth  <- 0.30  # [m] soil depth
+
+### Temporal Variables
+data.t_step <- hour
+
+# Field capacity calculation to use as input
+psi_fc  <- 33
+b       <- 2.91 + 15.9 * clay                 # [] b parameter (Campbell 1974) as in Cosby  et al. 1984 - Alternatively: obtain from land model.
+psi_sat <- exp(6.5 - 1.3 * sand) / 1000       # [kPa] saturation water potential (Cosby et al. 1984 after converting their data from cm H2O to Pa) - Alternatively: obtain from land model.
+fc      <- phi * (psi_sat / psi_fc)^(1 / b)   # [m3 m-3] Field capacity relative water content (water retention formula from Campbell 1984) - Alternatively: obtain from land model.
 
 # forcing.data   <- read.csv("input_forcing.csv") # forcing data file
-forcing.data <- data.frame(hour=c(1,2), temp = c(293.15, 293.15), moist=c(0.40, 0.40))
-forcing.data[, 1] <- forcing.data[, 1] * (data.time / tstep) # convert time units
+forcing.data <- data.frame(hour=c(1,2), temp = c(293.15, 293.15), moist=c(fc, fc))
+forcing.data[, 1] <- forcing.data[, 1] * (data.t_step / tstep) # convert time units
 names(forcing.data)[1] <- t_step
 
 # litter.data    <- read.csv("input_litter.csv") # litter input rates file
 litter.data <- data.frame(hour=c(1,2), litter_str = c(0.00015, 0.00015), litter_met = c(0.00001, 0.00001))
-litter.data[, 1] <- litter.data[, 1] * (data.time / tstep) # convert time units 
+litter.data[, 1] <- litter.data[, 1] * (data.t_step / tstep) # convert time units 
 names(litter.data)[1] <- t_step
-litter.data[,-1] <- litter.data[,-1] / data.time * tstep # convert litter input rates to the model time step rate
+litter.data[,-1] <- litter.data[,-1] / data.t_step * tstep # convert litter input rates to the model time step rate
 
-
-# ### Spatial Variables ==========================================================
-# clay   <- 0.51  # [g g^-1] clay fraction values 
-# sand   <- 0.03  # [g g^-1] sand fraction values 
-# silt   <- 0.46  # [g g^-1] silt fraction values 
-# phi    <- 0.5   # [m^3 m^-3] soil pore space
-# depth  <- 0.30  # [m] soil depth
