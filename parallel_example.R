cc993fb8146c84882b1295580bba388cfc379add
femoyano <fmoyano@uni-goettingen.de>
2016-03-04 10:25:23 +0100

    parallel example

--- parallel_example.R
+++ parallel_example.R
@@ -1,11 +1,43 @@
-library(doParallel)
-library(foreach)
+# Run only one chunk. Comment out the rest
 
+# For MPI jobs
+library(foreach)
 library(doMPI)
 cl <- startMPIcluster()
 registerDoMPI(cl)
 cat("Step 1")
 cat(clusterSize(cl))
+closeCluster(cl)
+mpi.quit()
+
+
+# For SMP jobs
+library(randomForest)
+library(doParallel)
+corenum <- detectCores()
+print(cornum)
+registerDoParallel(cores=corenum)
+x <- matrix(runif(500), 100)
+y <- gl(2, 50)
+st <- proc.time()
+rf <- foreach(ntree=rep(30000, 4), .combine=combine) %do% randomForest(x, y, ntree=ntree)
+proc.time()-st
+st <- proc.time()
+rf <- foreach(ntree=rep(30000, 4), .combine=combine, .packages='randomForest') %dopar% randomForest(x, y, ntree=ntree)
+proc.time()-st
+rm(rf)
+
+# x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+# trials <- 10000
+# ptime <- system.time({
+#   r <- foreach(icount(trials), .combine=cbind) %dopar% {
+#     ind <- sample(100, 100, replace=TRUE)
+#     result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+#     coefficients(result1)
+#   }
+# })[3]
+# ptime
+
 # Model = foreach(i=1:clusterSize(cl),.options.mpi = list(seed=1337) ) %dopar% {
 #   
 #   cat("Step 2")
@@ -22,35 +54,3 @@ cat(clusterSize(cl))
 # library(coda)
 # mcmclist = mcmc.list(lapply(Model,FUN=function(x) { x$Sol}))
 # save(Model,mcmclist, file = "Model.rdata")
-closeCluster(cl)
-mpi.quit()
-
-
-
-# library(randomForest)
-# 
-# registerDoParallel(cores=2)
-# 
-# x <- matrix(runif(500), 100)
-# y <- gl(2, 50)
-# 
-# st <- proc.time()
-# rf <- foreach(ntree=rep(30000, 4), .combine=combine) %do% randomForest(x, y, ntree=ntree)
-# proc.time()-st
-# 
-# st <- proc.time()
-# rf <- foreach(ntree=rep(30000, 4), .combine=combine, .packages='randomForest') %dopar% randomForest(x, y, ntree=ntree)
-# proc.time()-st
-# 
-# rm(rf)
-# 
-# x <- iris[which(iris[,5] != "setosa"), c(1,5)]
-# trials <- 10000
-# ptime <- system.time({
-#   r <- foreach(icount(trials), .combine=cbind) %dopar% {
-#     ind <- sample(100, 100, replace=TRUE)
-#     result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-#     coefficients(result1)
-#   }
-# })[3]
-# ptime
