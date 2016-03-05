d04f4faa0c3548c1b8f604f80641bc2d3b754c84
femoyano <fmoyano@uni-goettingen.de>
2016-03-05 18:16:18 +0100

    parallel test file

--- parallel_example.R
+++ parallel_example.R
@@ -1,47 +1,25 @@
 # Run only one chunk. Comment out the rest
 
 # # For MPI jobs
-# library(foreach)
-# library(doMPI)
-# cl <- startMPIcluster()
-# registerDoMPI(cl)
-# cat("Step 1")
-# cat(clusterSize(cl))
-# closeCluster(cl)
-# mpi.quit()
+library(foreach)
+library(doParallel)
+cl <- startMPIcluster()
+registerDoMPI(cl)
+cat("Step 1 \n")
+cat("cat clusterSize function here: ", clusterSize(cl))
+print("print clusterSize function here: ", clusterSize(cl))
+cat("cat detectCores ", detectCores())
+print("print detectCores ", detectCores())
+closeCluster(cl)
+mpi.quit()
 
 
 # For SMP jobs
-library(doParallel)
-corenum <- detectCores()
-registerDoParallel(cores=corenum)
-print(corenum)
-print(getDoParWorkers())
+# library(doParallel)
+# corenum <- detectCores()
+# registerDoParallel(cores=corenum)
+# cat("cat detectCores ", detectCores())
+# print("print detectCores ", detectCores())
+# print(getDoParWorkers())
 
-# x <- iris[which(iris[,5] != "setosa"), c(1,5)]
-# trials <- 10000
-# ptime <- system.time({
-#   r <- foreach(icount(trials), .combine=cbind) %dopar% {
-#     ind <- sample(100, 100, replace=TRUE)
-#     result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-#     coefficients(result1)
-#   }
-# })[3]
-# ptime
 
-# Model = foreach(i=1:clusterSize(cl),.options.mpi = list(seed=1337) ) %dopar% {
-#   
-#   cat("Step 2")
-#   library(MCMCglmm)
-#   load("mydata.rdata")
-#   nitt = 7000; thin = 50; burnin = 3000
-#   MCMCglmm( outcome ~ pred ,
-#             random=~idParents,
-#             family="poisson", 
-#             data=mydata, 
-#             pr = F, saveX = T, saveZ = T,
-#             nitt=nitt,thin=thin,burnin=burnin)
-# }
-# library(coda)
-# mcmclist = mcmc.list(lapply(Model,FUN=function(x) { x$Sol}))
-# save(Model,mcmclist, file = "Model.rdata")
