78aed659ecbd3ad17febad8d79289d60a8befea5
Fernando Moyano <fmoyano@uni-goettingen.de>
2016-06-23 18:18:13 +0200

    flag option for MM-uptake added. Set to false.

--- Model_desolve.R
+++ Model_desolve.R
@@ -66,8 +66,13 @@ Model_desolve <- function(t, initial_state, pars) { # must be defined as: func <
     # Microbial growth, mortality, respiration and enzyme production
     
     Diff.cd <- D_d * (C_D - 0)  # Calculate the diffusion fluxes
-    U.cd <- Uptake(Diff.cd, V_U, K_U, moist.mod, depth, fc.mod)  # Calculate the uptake flux
-
+    
+    if(flag.mmu) {
+      U.cd <- Uptake(Diff.cd, V_U, K_U, moist.mod, depth, fc.mod)  # Calculate the uptake flux    
+    } else {
+      U.cd <- Diff.cd
+    }
+  
     if(flag.mic) {
       F_cd.cm <- U.cd * f_gr * (1 - f_ep)
       F_cm.cp <- C_M * r_md * (1 - f_mr)
