af6902e0a53acc6fc08d13d64b15732057f4f6d6
Fernando Moyano <fmoyano@uni-goettingen.de>
2016-06-27 15:50:48 +0200

    Changes for single run, analysis and plotting.

--- analysis_with_run.R
+++ analysis_with_run.R
@@ -1,8 +1,68 @@
-### Analysis without run.
+### ===================================== ###
+### Analyze and plot model vs observation ###
+### ===================================== ###
 
-## ------------------------------------------------- ##
-##            Get accumulated values               ----
-## ------------------------------------------------- ##
+### Libraries
+require(deSolve)
+require(FME)
+require(plyr)
+require(reshape2)
+library(doParallel)
+
+cores = detectCores()
+registerDoParallel(cores = cores)
+
+
+## ------------------------------------ ##
+##  Check parameter sensitivities     ----
+## ------------------------------------ ##
+
+# par_corr_plot <- pairs(Sfun, which = c("C_R"), col = c("blue", "green"))
+# ident <- collin(Sfun)
+# ident_plot <- plot(ident, ylim=c(0,20))
+# ident[ident$N==9 & ident$collinearity<15,]
+
+
+## ------------------------------------ ##
+### -------- Get model data ----------- ##           
+## ------------------------------------ ##
+
+# Prepare setup
+
+list2env(setup, envir = .GlobalEnv)
+
+# pars_optim <- "-nb-nomic-nomin-v1"
+# diff.fun <- "hama"
+
+### Define time variables
+year     <- 31104000 # seconds in a year
+hour     <- 3600     # seconds in an hour
+sec      <- 1        # seconds in a second!
+# Other settings
+tstep <- get(t_step)
+tsave <- get(t_save)
+spinup     <- FALSE
+eq.stop    <- FALSE   # Stop at equilibrium?
+# Input Setup -----------------------------------------------------------------
+input_path    <- file.path(".")  # ("..", "NadiaTempMoist")
+data.samples  <- read.csv(file.path(input_path, sample_list_file))
+input.all     <- read.csv(file.path(input_path, "mtdata_model_input.csv"))
+obs.accum     <- read.csv(file.path(input_path, "mtdata_co2.csv"))
+site.data.mz  <- read.csv(file.path(input_path, "site_Closeaux.csv"))
+site.data.bf  <- read.csv(file.path(input_path, "site_BareFallow42p.csv"))
+obs.accum <- obs.accum[obs.accum$sample %in% data.samples$sample,]
+### Sourced required files ----------------------------------------------------
+source("parameters.R")
+source(paste("optim_pars", pars_optim, ".R", sep = ""))
+source("flux_functions.R")
+source("Model_desolve.R")
+source("Model_stepwise.R")
+source("initial_state.R")
+source("GetModelData.R")
+
+# Get model output with optimized parameters
+system.time(mod.out <- GetModelData(fitMod$par))
+
 # Get accumulated values to match observations and merge datasets
 data.accum <- merge(obs.accum, AccumCalc(mod.out, obs.accum), by.x = c("sample", "hour"), by.y = c("sample", "time"))
 data.accum$C_R_o <- data.accum$C_R
@@ -12,6 +72,7 @@ data.accum$C_R <- NULL
 ## ------------------------------------------------- ##
 ##  Do some statistics and simple comparisons      ----
 ## ------------------------------------------------- ##
+
 # Get RMSE and MAE
 res <- data.accum$C_R_o - data.accum$C_R_m
 RMSE <- sqrt(mean(res^2))
@@ -21,6 +82,7 @@ MAE <- mean(abs(res))
 ## ------------------------------------------------- ##
 ##         Calculate rates and normalize           ----
 ## ------------------------------------------------- ##
+
 # Calculate the rates for each accumulated period (* 1000 converts to mg)
 data.accum$C_R_or <- data.accum$C_R_o / data.accum$time_accum * 1000
 data.accum$C_R_mr <- data.accum$C_R_m / data.accum$time_accum * 1000
@@ -46,7 +108,7 @@ data.accum$C_R_norm <- NULL
 
 
 ## ------------------------------------------------- ##
-##         Fit simple nl models                    ----
+##         Fit simple nl models and plot           ----
 ## ------------------------------------------------- ##
 cv <- rainbow(10, alpha = 0.5)  # heat.colors(10, alpha = 0.5)
 palette(cv)
@@ -56,12 +118,12 @@ data.accum$moist.group <- interaction(data.accum$site, data.accum$moist_vol) # c
 
 # Fit a moisture function to each temperature subgroup and plot
 FitMoist <- function(df, var) {
-  df$C_R <- df[[var]]
-  fit <- nls(C_R ~ Rmax * ifelse((moist_vol - Th) < 0, 0, (moist_vol - Th)^2 / (K^2 + (moist_vol - Th)^2)),
-             start=c(Rmax=1, Th=0.1, K=0.05), lower=c(Rmax=0.5, Th=0, K=0.01),
-             upper=c(Rmax=1.5, Th=0.2, K=0.25), algorithm="port", data = df)
-  return(list(fit = fit, Rmax = coef(fit)[1], Th = coef(fit)[2], K = coef(fit)[3],
-              site = df$site[1], temp = df$temp[1]))
+    df$C_R <- df[[var]]
+    fit <- nls(C_R ~ Rmax * ifelse((moist_vol - Th) < 0, 0, (moist_vol - Th)^2 / (K^2 + (moist_vol - Th)^2)),
+               start=c(Rmax=1, Th=0.1, K=0.15), lower=c(Rmax=0.5, Th=0, K=0.01),
+               upper=c(Rmax=1.5, Th=0.2, K=0.25), algorithm="port", data = df)
+    return(list(fit = fit, Rmax = coef(fit)[1], Th = coef(fit)[2], K = coef(fit)[3],
+                site = df$site[1], temp = df$temp[1]))
 }
 fit.moist.obs <- dlply(data.accum, .(temp.group), .fun = FitMoist, var = "C_R_orn")
 fit.moist.mod <- dlply(data.accum, .(temp.group), .fun = FitMoist, var = "C_R_mrn")
@@ -70,14 +132,13 @@ fit.moist.mod <- dlply(data.accum, .(temp.group), .fun = FitMoist, var = "C_R_mr
 FitTemp <- function(df, var) {
   df$C_R <- df[[var]]
   fitEa <- nls(C_R ~ C_R_ref * exp(-Ea/0.008314*(1/(temp+273) - 1/273)),
-               start=c(C_R_ref = 0.1, Ea = 60), algorithm="port", data = df)
+             start=c(C_R_ref = 0.1, Ea = 60), algorithm="port", data = df)
   fitQ10 <- nls(C_R ~ C_R_ref * Q10^((temp-20)/10),
-                start=c(C_R_ref = 0.1, Q10 = 2), lower = c(C_R_ref = 0, Q10 = 1),
-                upper = c(C_R_ref = Inf, Q10 = Inf),
-                algorithm="port", data = df)
+               start=c(C_R_ref = 0.1, Q10 = 2), lower = c(C_R_ref = 0, Q10 = 1),
+               upper = c(C_R_ref = Inf, Q10 = Inf),
+               algorithm="port", data = df)
   return(list(fitEa = fitEa, fitQ10 = fitQ10, C_R_ref = coef(fitEa)[1], Ea = coef(fitEa)[2],
               Q10 = coef(fitQ10)[2], site = df$site[1], moist_vol = df$moist_vol[1]))
 }
 fit.temp.obs <- dlply(data.accum, .(moist.group), .fun = FitTemp, var = "C_R_or")
 fit.temp.mod <- dlply(data.accum, .(moist.group), .fun = FitTemp, var = "C_R_mr")
-
---
+++ run_smp.R
@@ -0,0 +1,137 @@
+#### optim_run_smp.R
+
+#### Documentations ===========================================================
+# Script used to run optimization as shared memory job (SMP)
+# author(s):
+# Fernando Moyano (fmoyano #at# uni-goettingen.de)
+#### ==========================================================================
+
+### ----------------------------------- ###
+###       User Stup                     ###
+### ----------------------------------- ###
+
+# Setup
+setup <- list(
+  # -------- Model options ----------
+  flag.ads  = 0 ,  # simulate adsorption desorption
+  flag.mic  = 0 ,  # simulate microbial pool explicitly
+  flag.fcs  = 1 ,  # scale C_P, C_A, C_Es, M to field capacity (with max at fc)
+  flag.sew  = 0 ,  # calculate C_E and C_D concentration in water
+  flag.dte  = 0 ,  # diffusivity temperature effect on/off
+  flag.dce  = 0 ,  # diffusivity carbon effect on/off
+  flag.mmu  = 0 ,  # michalis menten kinetics for uptake, else equal diffusion flux
+  dce.fun  = "exp"   ,  # diffusivity carbon function: 'exp' = exponential, 'lin' = linear
+  diff.fun = "cubic" ,  # Options: 'hama', 'cubic'
+  
+  # -------- Calibration options ----------
+  # Cost calculation type.
+  # Options: 'uwr' = unweighted residuals, 'wr' = wieghted residuals,  "rate.sd", "rate.mean"...
+  cost.type = "rate.mean" ,
+  # Which samples to run? E.g. samples.csv, samples_smp.csv, samples_4s.csv, samples_10s.csv
+  sample_list_file = "samples_smp.csv" ,
+  # Set of parameters initial values and bounds. Names must have: 
+  # -nb/-wb (narrow bounds or wide bounds), -v1/-v2/...
+  pars_replace = "pars_test1" ,
+  # Choose method for modFit
+  mf.method = "Nelder-Mead"
+)
+
+
+### ----------------------------------- ###
+###    Setings for parallel processing  ###
+### ----------------------------------- ###
+library(doParallel)
+cores = detectCores()
+# cores = 1
+cat("Cores detected:", cores, "\n")
+registerDoParallel(cores = cores)
+
+### Libraries =================================================================
+
+require(deSolve)
+require(FME)
+require(plyr)
+require(reshape2)
+
+list2env(setup, envir = .GlobalEnv)
+
+### Define time variables =====================================================
+year     <- 31104000 # seconds in a year
+hour     <- 3600     # seconds in an hour
+sec      <- 1        # seconds in a second!
+
+# ----- fixed model setup ----
+t_step     <- "hour"  # Model time step (as string). Important when using stepwise run.
+t_save     <- "hour"  # save time step (only for stepwise model?)
+ode.method <- "lsoda"  # see ode function
+flag.des   <- 1       # Cannot be changed: model crashes when doing stepwise.
+tstep      <- get(t_step)
+tsave      <- get(t_save)
+spinup     <- FALSE
+eq.stop    <- FALSE   # Stop at equilibrium?
+runname <- paste("RUN_", pars_replace, sep="")
+options <- paste("-ads", flag.ads, "_mic", flag.mic, "_fcs", flag.mmu, "_mmu", flag.fcs, "_sew", flag.sew,
+                 "_dte", flag.dte, "_dce", flag.dce, "_", dce.fun, "_", diff.fun,
+                 "_", mf.method, "_", cost.type, "-", sep = "")
+
+# Input Setup -----------------------------------------------------------------
+input_path    <- file.path(".")  # ("..", "NadiaTempMoist")
+data.samples  <- read.csv(file.path(input_path, sample_list_file))
+input.all     <- read.csv(file.path(input_path, "mtdata_model_input.csv"))
+obs.accum     <- read.csv(file.path(input_path, "mtdata_co2.csv"))
+site.data.mz  <- read.csv(file.path(input_path, "site_Closeaux.csv"))
+site.data.bf  <- read.csv(file.path(input_path, "site_BareFallow42p.csv"))
+
+obs.accum <- obs.accum[obs.accum$sample %in% data.samples$sample,]
+
+### Sourced required files ----------------------------------------------------
+load("../NadiaTempMoist/parameters.RData")
+pars_new <- get(pars_replace)
+source("parameters.R")
+source("flux_functions.R")
+source("Model_desolve.R")
+source("initial_state.R")
+source("ModCost_byMoist.R")
+source("AccumCalc.R")
+source("ParsReplace.R")
+source("SampleRun.R")
+source("GetModelData.R")
+
+
+### ----------------------------------- ###
+###              Single Run             ###
+### ----------------------------------- ###
+t0 <- Sys.time()
+
+pars <- ParsReplace(pars_new, pars)
+
+mod.out <- foreach(i = data.samples$sample, .combine = 'rbind',
+                   .export = c(ls(envir = .GlobalEnv), "pars"),
+                   .packages = c("deSolve")) %dopar% {
+                     SampleRun(pars, data.samples[data.samples$sample==i, ],
+                               input.all[input.all$sample==i, ])
+                   }
+
+print(Sys.time() - t0)
+
+# ### ----------------------------------- ###
+# ###        Analysis and Plotting        ###
+# ### ----------------------------------- ###
+
+source("analysis.R")
+
+source("analysis_plots.R")
+
+# ### ----------------------------------- ###
+# ###        Saving work space            ###
+# ### ----------------------------------- ###
+# 
+# savetime  <- format(Sys.time(), "%m%d-%H%M")
+# 
+# rm(list=names(setup), year, hour, sec, tstep, tsave, spinup, eq.stop, input.all,
+#    site.data.bf, site.data.mz, initial_state, obs.accum)
+# 
+# save.image(file = paste(runname, options, savetime, ".RData", sep = ""))
+
+
+
