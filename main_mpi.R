72447a5e1764dad29e94a82e428f9ed9ea03b4d0
Fernando Moyano <fmoyano@uni-goettingen.de>
2016-07-01 16:08:50 +0200

    Changes for mpi run.

---
+++ main_mpi.R
@@ -0,0 +1,92 @@
+#### optim_run_main.R
+
+#### Documentations ===========================================================
+# Script used to prepare settings and run parameter optimization
+# author(s):
+# Fernando Moyano (fmoyano #at# uni-goettingen.de)
+#### ==========================================================================
+
+
+### ----------------------------------- ###
+###      Non User Setup                 ###
+### ----------------------------------- ###
+
+### Libraries =================================================================
+
+# require(deSolve)
+# require(FME)
+# require(plyr)
+# require(reshape2)
+
+### Define time variables =====================================================
+year     <- 31104000 # seconds in a year
+hour     <- 3600     # seconds in an hour
+sec      <- 1        # seconds in a second!
+
+# ----- fixed model setup ----
+t_step     <- "hour"  # Model time step (as string). Important when using stepwise run.
+t_save     <- "hour"  # save time step (only for stepwise model?)
+ode.method <- "lsoda"  # see ode function
+flag.des   <- 1       # Cannot be changed: model crashes when doing stepwise.
+tstep      <- get(t_step)
+tsave      <- get(t_save)
+spinup     <- FALSE
+eq.stop    <- FALSE   # Stop at equilibrium?
+
+
+# Input Setup -----------------------------------------------------------------
+input_path    <- file.path(".")  # ("..", "Analysis", "NadiaTempMoist")
+data.samples  <- read.csv(file.path(input_path, sample_list_file))
+input.all     <- read.csv(file.path(input_path, "mtdata_model_input.csv"))
+obs.accum     <- read.csv(file.path(input_path, "mtdata_co2.csv"))
+site.data.mz  <- read.csv(file.path(input_path, "site_Closeaux.csv"))
+site.data.bf  <- read.csv(file.path(input_path, "site_BareFallow42p.csv"))
+
+obs.accum <- obs.accum[obs.accum$sample %in% data.samples$sample,]
+
+### Sourced required files ----------------------------------------------------
+source("flux_functions.R")
+source("Model_desolve.R")
+source("Model_stepwise.R")
+source("initial_state.R")
+source("ModCost_SR_TR.R")
+source("AccumCalc.R")
+source("ParsReplace.R")
+source("SampleRun.R")
+source("GetModelData.R")
+
+
+### ----------------------------------- ###
+###      Optimization/Calibration       ###
+### ----------------------------------- ###
+
+# ### Check model cost and computation time --------------
+# system.time(cost <- ModCost(pars_optim_init))
+# 
+# ### Check sensitivity of parameters ---------------
+# Sfun <- sensFun(ModCost, pars_optim_init)
+
+## Optimize parameters
+ModCost(pars_optim_init)
+
+savetime  <- format(Sys.time(), "%m%d-%H%M")
+
+save.image(file = paste(runname, options, savetime, ".RData", sep = ""))
+
+# ## Run Bayesian optimization
+# var0 = obs.accum$sd.r
+#  
+# mcmcMod <- modMCMC(f=ModCost, p=fitMod$par, niter=5000, var0=var0,
+#                    lower=pars_optim_lower, upper=pars_optim_upper, updatecov = 100)
+# 
+# 
+# ### ----------------------------------- ###
+# ###        Saving work space            ###
+# ### ----------------------------------- ###
+# 
+# savetime  <- format(Sys.time(), "%m%d-%H%M")
+# 
+# rm(list=names(setup), year, hour, sec, tstep, tsave, spinup, eq.stop, input.all,
+#    site.data.bf, site.data.mz, initial_state, obs.accum)
+# 
+# save.image(file = paste(runname, options, savetime, ".RData", sep = ""))
--- run_lhs_smp.R
+++ run_lhs_smp.R
@@ -6,45 +6,32 @@
 # Fernando Moyano (fmoyano #at# uni-goettingen.de)
 #### ==========================================================================
 
-### ----------------------------------- ###
-###    Setings for parallel processing  ###
-### ----------------------------------- ###
-library(doParallel)
-# cores = detectCores()
-cores = 1
-cat("Cores detected:", cores, "\n")
-registerDoParallel(cores = cores)
-
-
 t0 <- Sys.time()
-
 
 ### ----------------------------------- ###
 ###       User Stup                     ###
 ### ----------------------------------- ###
 # Setup
-setup <- list(
-  # -------- Model options ----------
-  flag.ads  = 0 ,  # simulate adsorption desorption
-  flag.mic  = 1 ,  # simulate microbial pool explicitly
-  flag.fcs  = 1 ,  # scale C_P, C_A, C_Es, M to field capacity (with max at fc)
-  flag.sew  = 0 ,  # calculate C_E and C_D concentration in water
-  flag.dte  = 0 ,  # diffusivity temperature effect on/off
-  flag.dce  = 0 ,  # diffusivity carbon effect on/off
-  flag.mmu  = 1 ,  # michalis menten kinetics for uptake, else equal diffusion flux
-  flag.mmr  = 1 ,  # microbial maintenance respiration
-  dce.fun  = "exp"   ,  # diffusivity carbon function: 'exp' = exponential, 'lin' = linear
-  diff.fun = "hama" ,  # Options: 'hama', 'cubic'
-  
-  # -------- Calibration options ----------
-  # Cost calculation type.
-  # Options: 'uwr' = unweighted residuals, 'wr' = wieghted residuals,  "rate.sd", "rate.mean"...
-  cost.type = "rate.mean" ,
-  # Which samples to run? E.g. samples.csv, samples_smp.csv, samples_4s.csv, samples_10s.csv
-  sample_list_file = "samples_smp.csv" ,
-  # Choose method for modFit
-  mf.method = "Nelder-Mead"
-)
+# -------- Model options ----------
+flag.ads  <- 0     # simulate adsorption desorption
+flag.mic  <- 1     # simulate microbial pool explicitly
+flag.fcs  <- 1     # scale C_P, C_A, C_Es, M to field capacity (with max at fc)
+flag.sew  <- 0     # calculate C_E and C_D concentration in water
+flag.dte  <- 0     # diffusivity temperature effect on/off
+flag.dce  <- 0     # diffusivity carbon effect on/off
+flag.mmu  <- 1     # michalis menten kinetics for uptake, else equal diffusion flux
+flag.mmr  <- 1     # microbial maintenance respiration
+dce.fun  <- 'exp'  # diffusivity carbon function: 'exp' = exponential, 'lin' = linear
+diff.fun <- 'hama' # Options: 'hama', 'cubic'
+
+# -------- Calibration options ----------
+# Cost calculation type.
+# Options: 'uwr' = unweighted residuals, 'wr' = wieghted residuals,  'rate.sd', 'rate.mean'...
+cost.type <- 'rate.mean' 
+# Which samples to run? E.g. samples.csv, samples_smp.csv, samples_4s.csv, samples_10s.csv
+sample_list_file <- 'samples_smp.csv' 
+# Choose method for modFit
+mf.method <- 'Nelder-Mead'
 
 
 ### ----------------------------------- ###
@@ -52,13 +39,13 @@ setup <- list(
 ### ----------------------------------- ###
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Choose default parameters
-pars.default.file <- "parset6.csv"
+pars.default.file <- 'parset6.csv'
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Choose initial valeus for optimized parameters
-pars.calib.file   <- "pars_calib_lh10.csv"
+pars.calib.file   <- 'pars_calib_lh10.csv'
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Choose bounds R file
-pars.bounds.file <- "pars_bounds_v1.R"
+pars.bounds.file <- 'pars_bounds_v1.R'
 
 pars <- as.matrix(read.csv(pars.default.file))[1,]
 pars_calib <- as.matrix(read.csv(file=pars.calib.file))
@@ -66,13 +53,25 @@ source(pars.bounds.file)
 pars_optim_lower <- pars_bounds[1,]
 pars_optim_upper <- pars_bounds[2,]
 
+### ----------------------------------- ###
+###               Run                   ###
+### ----------------------------------- ###
+### ----------------------------------- ###
+###    Setings for parallel processing  ###
+### ----------------------------------- ###
 
-### ----------------------------------- ###
-###      Run multiple optimizations     ###   #### !!!!!! better use separate mpi runs rather than loop !!!!!
-### ----------------------------------- ###
-for (i in 1:nrow(pars_calib)) {
-  runname <- paste("MultRun", i, sep="")   
+library(doMPI)
+cl <- startMPIcluster()
+registerDoMPI(cl)
+cores <- clusterSize(cl)
+runs.out <- foreach(i = 1:nrow(pars_calib), 
+                    .combine = 'cbind', 
+                    .errorhandling = 'remove', 
+                    .packages = c('deSolve', 'plyr', 'reshape2')
+                    ) %dopar% {
+  runname <- paste('MpiRun', i, sep='')
   pars_optim_init <- pars_calib[i, ]
-  source("optim_main.R")
+  source('main_mpi.R')
 }
-
+closeCluster(cl)
+mpi.quit()
