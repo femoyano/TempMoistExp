f91fb16c2d5d9ffe8006ff53cce193f84c33af80
Fernando Moyano <fmoyano@uni-goettingen.de>
2016-07-04 15:54:57 +0200

    file renaming

--- run_openmpi.sh
+++ run_openmpi.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #BSUB -a openmpi
 #BSUB -q mpi
-#BSUB -W 4:00
-#BSUB -n 80
-#BSUB -o %J.out.mpi
+#BSUB -W 12:00
+#BSUB -n 101,110
+#BSUB -o out.%J.mpi.out
 
 module load intel/mkl/64/11.2/2015.3.187 && module load openmpi/intel
 
-mpirun.lsf Rscript --slave "optim_run_mpi.R"
+mpirun.lsf Rscript --slave "run_mpi.R"
