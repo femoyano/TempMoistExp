d69e481ef1b448c9c7c28beaf30b801dde9e09fc
femoyano <fmoyano@uni-goettingen.de>
2017-01-23 15:29:15 +0100

    new diffusion functions to test

--- flux_functions.R
+++ flux_functions.R
@@ -10,8 +10,7 @@
 #### Fernando Moyano (fmoyano #at# uni-goettingen.de)
 #### ==========================================================================
 
-## Functions that get moisture modifiers depending on the
-## options ----
+## Functions that get moisture modifiers depending on the options ----
 if (flag.fcs) {
   get.fc.mod <- function(moist, fc) min(1, moist/fc)
 } else {
@@ -26,13 +25,40 @@ if (flag.sew) {
 
 ## Functions to calculate diffusion depending on options -----
 if (diff.fun == "hama") {
-  get.D_sm <- function(moist, ps, Rth) if (moist <= Rth) {D_sm <- 0} else
-     D_sm <- (ps - Rth)^1.5 * ((moist - Rth)/(ps - Rth))^2.5
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) {
+    if (moist <= Rth) {D_sm <- 0} else
+     D_sm <- (ps - Rth)^p1 * ((moist - Rth) / (ps - Rth))^p2 # p1=1.5, p2=2.5
+  }
+}
+if (diff.fun == "oles1") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) {
+    if (moist < 0.022*b) {D_sm <- 0} else
+    D_sm <- p1 * moist * (moist - 0.022 * b) / (ps - 0.022 * b) # p1=0.45
+  }
+}
+if (diff.fun == "oles2") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) {
+    D_sm <- p1 * moist * (moist / ps)^(p2*b) # p1=0.45, p2=0.3
+  }
 }
 if (diff.fun == "cubic") {
-  get.D_sm <- function(moist, ps, Rth) D_sm <- moist^3
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) D_sm <- moist^p1  # p1=3
+}
+if (diff.fun == "MQ") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) D_sm <- moist^p1 / ps^p2 # p1=3.33, p2=2
+}
+if (diff.fun == "PC") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) D_sm <- p1*moist^p2  # p1=2.8, p2=3
+}
+if (diff.fun == "sade") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) D_sm <- p1*(moist/ps)^p2  # p1=0.73, p2=1.98
+}
+if (diff.fun == "archie") {
+  get.D_sm <- function(moist, ps, Rth, b, p1, p2) D_sm <- ps^p1 * (moist/ps)^p2  # p1=1.3-2.25, p2=2-2.5
 }
 
+
+## Other factors affectign diffusion diffusion -----
 if (flag.dte) {
   get.D_tm <- function(temp, T_ref) temp^8/T_ref^8
 } else get.D_tm <- function(temp, T_ref) 1
